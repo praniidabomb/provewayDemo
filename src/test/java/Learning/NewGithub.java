25a085cd261c2dc0ed460ff67cbcc6a6ce1e27b9
Pranay Vithlani <pranay2410@gmail.com>
2025-02-09 18:36:37 +0530

    See this code on the Github repo link online

---
+++ src/test/java/Learning/NewGithub.java
@@ -0,0 +1,8 @@
+package Learning;
+
+public class NewGithub {
+    public static void main(String args[])
+    {
+        System.out.println("Writing this specifically to see it on the git hub repository");
+    }
+}
